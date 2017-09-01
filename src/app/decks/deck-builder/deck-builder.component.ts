e83bf63666c9fd491e017cd46d895994b788b2db
Martin Hylleberg <mhylle@yahoo.com>
2017-09-02 00:06:51 +0200

    unit type shown on ui

--- src/app/decks/deck-builder/deck-builder.component.ts
+++ src/app/decks/deck-builder/deck-builder.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UnitsService} from "../../game/units/units.service";
 import {Unit} from "../../model/units/Unit";
+import {UnitType} from "../../model/units/UnitType";
 
 @Component({
   selector: 'app-deck-builder',
@@ -11,7 +12,7 @@ import {Unit} from "../../model/units/Unit";
 export class DeckBuilderComponent implements OnInit {
 
   units: Unit[];
-
+  UnitType: typeof UnitType = UnitType;
   constructor(private unitService: UnitsService) {
     this.units = this.unitService.getUnits();
   }
--- src/app/game/units/mock-units.ts
+++ src/app/game/units/mock-units.ts
@@ -22,8 +22,8 @@ export const UNITS: any[] = [
     y: 0,
     posX: '0px',
     posY: '0px',
-    w: 1,
-    h: 1,
+    w: 2,
+    h: 2,
     hp: 256,
     level: 1
 
